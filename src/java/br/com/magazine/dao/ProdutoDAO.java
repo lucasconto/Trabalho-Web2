40a0a7e141de3ab06221a44ede5106227a3c727d
brunosella <brunosella@192.168.0.104>
2015-06-04 16:38:14 -0300

    Update de Produto

--- src/java/br/com/magazine/dao/ProdutoDAO.java
+++ src/java/br/com/magazine/dao/ProdutoDAO.java
@@ -19,7 +19,8 @@ public class ProdutoDAO {
     //sem imagem
     private final String stmtCadastraProduto = "insert into produto (titulo, autor, editora, categoria, preco, genero) values (?,?,?,?,?,?)";
     //private final String stmtCadastraProduto = "insert into produto (titulo, autor, editora, categoria, preco, genero, idImg) values (?,?,?,?,?,?)";
-    
+    private final String stmtAtualizaProduto = "update produto set titulo = ?, autor = ?, editora = ?, categoria = ?, preco = ?, genero = ? where idProduto = ?";
+
     public void cadastrarProduto (Produto p) throws ClassNotFoundException{ 
         Connection con = null;
         PreparedStatement stmt = null;
@@ -51,5 +52,37 @@ public class ProdutoDAO {
                 System.out.println("Erro ao fechar a conexao. Ex = " + ex.getMessage());
             }
         }
+        
     }
-}
+        public void atualizarProduto(Produto p) throws ClassNotFoundException {
+        Connection con = null;
+        PreparedStatement stmt = null;
+        try {
+            con = ConnectionFactory.getConnection();
+            con.setAutoCommit(false);
+            stmt = con.prepareStatement(stmtAtualizaProduto);
+            stmt.setString(1, p.getTitulo());
+            stmt.setString(2, p.getAutor());
+            stmt.setString(3, p.getEditora());
+            stmt.setString(4, p.getCategoria());
+            stmt.setDouble(5, p.getPreco());
+            stmt.setString(6, p.getGenero().getNome());
+            stmt.setInt(7, p.getIdProduto());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                stmt.close();
+            } catch (SQLException ex) {
+                System.out.println("Erro ao fechar statement. Ex = " + ex.getMessage());
+            }
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                System.out.println("Erro ao fechar a conexao. Ex = " + ex.getMessage());
+            }
+        }
+    }
+        
+    }
