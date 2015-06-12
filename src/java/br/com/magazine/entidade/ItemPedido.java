5e1e09b35ba510f71ae8cecaa9f8d3cac14c31fe
Evandro <Evandro@Evandro-PC>
2015-06-12 17:53:20 -0300

    Erro ao renomear arquivo parte 2 de 2. Agora tudo beleza!

---
+++ src/java/br/com/magazine/entidade/ItemPedido.java
@@ -0,0 +1,58 @@
+package br.com.magazine.entidade;
+
+public class ItemPedido {
+    int idItemPedido;
+    int idPedido;
+    private Produto produto;
+    int quantidade;
+    double valorUnitario;
+   
+    public int getIdItemPedido() {
+        return idItemPedido;
+    }
+
+    public void setIdItemPedido(int idItemPedido) {
+        this.idItemPedido = idItemPedido;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    /**
+     * @return the produto
+     */
+    public Produto getProduto() {
+        return produto;
+    }
+
+    /**
+     * @param produto the produto to set
+     */
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    
+    
+}
